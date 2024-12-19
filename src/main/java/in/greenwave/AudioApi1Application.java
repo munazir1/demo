7e8206f0daff701e1bbe839b392d091968b1663a
munazir21 <92munazir@gmail.com>
2024-12-19 17:56:45 +0530

    demo

---
+++ src/main/java/in/greenwave/AudioApi1Application.java
@@ -0,0 +1,13 @@
+package in.greenwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioApi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/in/greenwave/AudioApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.greenwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
